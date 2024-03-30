a7111b202d901f584fe68df2cf3b98c902d659fc
yibeizzZ <143059475+yibeizzZ@users.noreply.github.com>
2024-03-31 02:20:18 +0800

    1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow( Model *model ,QWidget *parent)
+MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
-    , allModel(model)
+
 {
     ui->setupUi(this);
 
